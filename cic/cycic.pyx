e0e739f17fb9d7e7b8b042b5cf05947acf9f3a2f
Lizhou Sha <slz@mit.edu>
2016-06-09 22:39:08 -0400

    Rewrote cycic.cic_3d main loop to be performant.
    Introduces sanitizing function in cycic.
    
    Signed-off-by: Lizhou Sha <slz@mit.edu>

--- cic/cycic.pyx
+++ cic/cycic.pyx
@@ -21,35 +21,45 @@ def cic_3d(np.ndarray[FLOAT_t, ndim=2] points, np.ndarray[INT_t, ndim=1] ndims):
     np.ndarray points : (n,3) array of points.
     np.ndarray ndims : (3,) vector for the shape of the return value.
     """
-    if points.shape[1] != 3:
-        raise ValueError("Argument `points` must of shape (:,3).")
-    elif (points < 0).any():
-        raise ValueError("Argument `points` must be positive.")
+    cic_sanitize(points, ndims)
     if ndims.shape[0] != 3:
-        raise ValueError("Argument `ndims` must have shape (3,).")
-    if (points >= ndims).any():
-        raise ValueError("Argument `ndims` too small to contain the range of `points`.")
-#    assert points.dtype == FLOAT and ndims.dtype == FLOAT
+        raise ValueError("Argument `ndims` must be of shape (3,).")
 
     ndensity = np.zeros(ndims + 2, dtype=FLOAT)
     cdef FLOAT_t[:, :, :] ndensity_view = ndensity
-#    cdef FLOAT_t[:, :] points_view = points
-    cdef np.ndarray[FLOAT_t, ndim=1] cell# = np.zeros(3, dtype=UINT)
-    cdef np.ndarray[FLOAT_t, ndim=2] delta = np.zeros((2,3), dtype=FLOAT)
-    cdef np.ndarray[FLOAT_t, ndim=1] celldelta# = np.zeros(3, dtype=FLOAT)
+    cdef UINT_t[:] cell = np.zeros(3, dtype=UINT)
+    cdef FLOAT_t[:, :] delta = np.zeros((2,3), dtype=FLOAT)
+    cdef FLOAT_t celldelta = 0
     cdef np.ndarray[FLOAT_t, ndim=1] p
     cdef UINT_t i, j, k
-    cdef unsigned long n
-#    for n in range(points.shape[0]):
-    for p in points:
+    cdef size_t n
+    for n in range(points.shape[0]):
         # Find the cell nearest to p
-#        p = points[n]
-        cell = np.rint(p)
-        celldelta = cell - p
-        delta[0] = 0.5 + celldelta
-        delta[1] = 0.5 - celldelta
+        for i in range(3):
+            cell[i] = <UINT_t>(points[n, i] + 0.5)
+            celldelta = cell[i] - points[n, i]
+            delta[0, i] = 0.5 + celldelta
+            delta[1, i] = 0.5 - celldelta
         for i in range(2):
             for j in range(2):
                 for k in range(2):
                     ndensity_view[<UINT_t>(cell[0]+i), <UINT_t>(cell[1]+j), <UINT_t>(cell[2]+k)] += delta[i,0] * delta[j,1] * delta[k,2]
     return ndensity
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cdef int cic_sanitize(FLOAT_t[:,:] points, INT_t[:] ndims):
+    if points.shape[1] != ndims.shape[0]:
+        raise ValueError("Dimension mismatch between arguements `points` and `ndims`.")
+
+    cdef size_t i, j
+    for i in range(ndims.shape[0]):
+        if ndims[i] <= 0:
+            raise ValueError("Argument `ndims` must be positive.")
+    for i in range(points.shape[0]):
+        for j in range(points.shape[1]):
+            if points[i,j] > ndims[j]:
+                raise ValueError("Argument `ndims` too small for the range of `points`")
+            elif points[i,j] < 0:
+                raise ValueError("Argument `points` must be nonnegative.")
+    return True
