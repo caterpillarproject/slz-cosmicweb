5b36102a7e23b9adb2e36452d61ee9a30b7b10b8
Lizhou Sha <slz@mit.edu>
2016-06-10 17:40:40 -0400

    Documented cycic.pyx better
    Signed-off-by: Lizhou Sha <slz@mit.edu>

--- cic/cycic.pyx
+++ cic/cycic.pyx
@@ -1,9 +1,11 @@
+%%cython -a
 from __future__ import division, print_function
 import math
 import numpy as np
 cimport numpy as np
 cimport cython
 
+# Define types to be used as array dtypes
 FLOAT = np.float
 INT = np.int_
 UINT = np.uint
@@ -18,28 +20,49 @@ def cic_3d(np.ndarray[FLOAT_t, ndim=2] points, np.ndarray[INT_t, ndim=1] ndims):
 
     Parameters
     ----------
-    np.ndarray points : (n,3) array of points.
-    np.ndarray ndims : (3,) vector for the shape of the return value.
+    points : np.ndarray[FLOAT_t, ndim=2]
+        Must be non-negative. `points.shape[1]` must be 3.
+    ndims : np.ndarray[FLOAT_t, ndim=2]
+        Desired number of cells per dimension. Must have length 3.
+
+    Returns
+    -------
+    ndensity : np.ndarray[FLOAT_t, ndim=2]
+        A 2D array of the number density per cell. It has shape (ndims + 2).
+
+    Raises
+    ------
+    ValueError
+        If any of the following conditions are not met:
+        points.shape[1] == ndims.shape[0]
+        points.shape[1] == 3
+        points.shape[1] == ndims.shape[0]
+        (ndims > 0).all()
+        (points <= ndims).all() and (points > 0).all()
     """
     cic_sanitize(points, ndims)
     if ndims.shape[0] != 3:
         raise ValueError("Argument `ndims` must be of shape (3,).")
 
+    # Pre-allocate variables to be used in the main loop
+    # ndensity has shape ndims + 2 in case points are within 0.5 of the box edge.
     ndensity = np.zeros(ndims + 2, dtype=FLOAT)
     cdef FLOAT_t[:, :, :] ndensity_view = ndensity
     cdef UINT_t[:] cell = np.zeros(3, dtype=UINT)
     cdef FLOAT_t[:, :] delta = np.zeros((2,3), dtype=FLOAT)
     cdef FLOAT_t celldelta = 0
-    cdef np.ndarray[FLOAT_t, ndim=1] p
     cdef UINT_t i, j, k
     cdef size_t n
+
+    # Loop over the number of points
     for n in range(points.shape[0]):
-        # Find the cell nearest to p
+        # Loop over each dimension of each point
         for i in range(3):
             cell[i] = <UINT_t>(points[n, i] + 0.5)
             celldelta = cell[i] - points[n, i]
             delta[0, i] = 0.5 + celldelta
             delta[1, i] = 0.5 - celldelta
+        # Loop over the 8 neighbors of each point
         for i in range(2):
             for j in range(2):
                 for k in range(2):
@@ -49,17 +72,41 @@ def cic_3d(np.ndarray[FLOAT_t, ndim=2] points, np.ndarray[INT_t, ndim=1] ndims):
 @cython.wraparound(False)
 @cython.boundscheck(False)
 cdef int cic_sanitize(FLOAT_t[:,:] points, INT_t[:] ndims):
+    """Checks inputs to cloud-in-cell functions for sanity.
+
+    Parameters
+    ----------
+    points : a 2D array of floats.
+    ndims : a 1D array of integers.
+
+    Returns
+    -------
+    int
+        True if all test passes
+
+    Raises
+    ------
+    ValueError
+        If any of the following conditions are not met:
+        points.shape[1] == ndims.shape[0]
+        (ndims > 0).all()
+        (points <= ndims).all() and (points > 0).all()
+    """
     if points.shape[1] != ndims.shape[0]:
         raise ValueError("Dimension mismatch between arguements `points` and `ndims`.")
+        return False
 
     cdef size_t i, j
     for i in range(ndims.shape[0]):
         if ndims[i] <= 0:
             raise ValueError("Argument `ndims` must be positive.")
+            return False
     for i in range(points.shape[0]):
         for j in range(points.shape[1]):
             if points[i,j] > ndims[j]:
                 raise ValueError("Argument `ndims` too small for the range of `points`")
+                return False
             elif points[i,j] < 0:
-                raise ValueError("Argument `points` must be nonnegative.")
+                raise ValueError("Argument `points` must be non-negative.")
+                return False
     return True
